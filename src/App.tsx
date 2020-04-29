6f72a84957f8050d425e9c82a63a538e91decaf1
Rangga-PR <ranggaputraraharja@gmail.com>
2020-04-29 22:57:57 +0700

    layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import styled from "styled-components";
+import { colorPalates } from "./variables";
+
+const AppContainer = styled.div`
+  text-align: center;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+  height: 100vh;
+  background-color: ${colorPalates.forestGreen};
+`;
+
+const Title = styled.h1`
+  margin: 0;
+  font-size: 48px;
+  color: white;
+`;
+
+const Input = styled.input`
+  margin-top: 25px;
+  border-radius: 5px;
+  width: 300px;
+  height: 20px;
+  padding: 7px;
+`;
+
+const StrengthGauge = styled.div`
+  height: 25px;
+  width: 318px;
+  border-radius: 5px;
+  background-color: white;
+  margin-top: 10px;
+`;
+
+const Divider = styled.div`
+  margin-top: 30px;
+  height: 2px;
+  width: 85%;
+  border-bottom: 2px solid white;
+
+  @media only screen and (min-width: 769px) {
+    width: 600px;
+  }
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppContainer>
+      <Title>Password Strength Checker</Title>
+      <Input type="text" placeholder="type your password here" />
+      <StrengthGauge />
+      <Divider />
+    </AppContainer>
   );
 }
 
---
+++ src/variables.js
@@ -0,0 +1,8 @@
+const colorPalates = {
+  forestGreen: "#217756",
+  paleGreen: "#63b75d",
+  yellow: "#fced25",
+  limeGreen: "#b0d553",
+};
+
+module.exports = { colorPalates };
